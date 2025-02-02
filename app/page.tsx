9e06b250b075e6f880b29b34113298a4a7110c03
vyom-cpp <vyom.sutariya0948@gmail.com>
2025-02-02 21:52:18 +0530

    code updation

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function Page() {
           </Markdown>
         </BlurFade>
       </section>
-      <section id="work">
+      {/* <section id="work">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 5}>
             <h2 className="text-xl font-bold">Work Experience</h2>
@@ -73,7 +73,7 @@ export default function Page() {
             </BlurFade>
           ))}
         </div>
-      </section>
+      </section> */}
       <section id="education">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 7}>
@@ -86,11 +86,11 @@ export default function Page() {
             >
               <ResumeCard
                 key={education.school}
-                href={education.href}
+                // href={education.href}
                 logoUrl={education.logoUrl}
                 altText={education.school}
                 title={education.school}
-                subtitle={education.degree}
+                // subtitle={education.degree}
                 period={`${education.start} - ${education.end}`}
               />
             </BlurFade>
